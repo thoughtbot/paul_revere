e07582fe3554ab42b35f0d7c0935640c51f445a7
Joe Ferris <joe.r.ferris@gmail.com>
2010-01-20 14:17:20 -0500

    Rearrange requires to avoid dependency issues on Rails 2.3.5

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,11 @@ ENV['RAILS_ENV'] = 'test'
 
 require 'rubygems'
 require 'shoulda'
-require 'active_support'
-require 'active_support/test_case'
 require 'action_controller'
 require 'action_view'
 require 'action_view/test_case'
+require 'active_support'
+require 'active_support/test_case'
 require 'test_help'
 require 'redgreen'
 
