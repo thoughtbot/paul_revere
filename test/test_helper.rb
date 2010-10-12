cf6dfb120ef1b3d97d6073f55af31affd7d63d26
Nick Quaranto <nick@quaran.to>
2010-10-12 17:42:51 -0400

    Passing suite for rails 3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,31 +1,15 @@
 ENV['RAILS_ENV'] = 'test'
 
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-RAILS_ROOT = PROJECT_ROOT
 
 require 'rubygems'
-require 'active_support'
-require 'active_support/test_case'
+require 'rails/all'
 require 'shoulda'
-require 'action_controller'
-require 'action_view'
-require 'action_view/test_case'
-require 'active_record'
-require 'test_help'
+require 'bourne'
 require 'redgreen'
 
-class ActionView::TestCase
-  class TestController < ActionController::Base
-    attr_accessor :request, :response, :params
-
-    def initialize
-      @request = ActionController::TestRequest.new
-      @response = ActionController::TestResponse.new
-
-      # TestCase doesn't have context of a current url so cheat a bit
-      @params = {}
-      send(:initialize_current_url)
-    end
+module PaulRevere
+  class Application < Rails::Application
   end
 end
 
@@ -35,12 +19,9 @@ class ActiveSupport::TestCase
   end
 end
 
-database_file = File.join(File.dirname(__FILE__), 'test.db')
-at_exit { FileUtils.rm(database_file) }
-
 load File.join(PROJECT_ROOT, 'generators', 'paul_revere', 'templates', 'migration.rb')
 ActiveRecord::Base.establish_connection(
   :adapter  => 'sqlite3',
-  :database => database_file
+  :database => ':memory:'
 )
 CreateAnnouncements.suppress_messages { CreateAnnouncements.migrate(:up) }
