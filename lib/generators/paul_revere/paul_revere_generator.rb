a061606ce927f2ba8d8bd6ef10cb60e740e08798
Jason Morrison <jmorrison@thoughtbot.com>
2011-05-11 11:54:50 -0400

    Update generator to be a Rails 3 generator

--- lib/generators/paul_revere/paul_revere_generator.rb
+++ lib/generators/paul_revere/paul_revere_generator.rb
@@ -1,10 +1,14 @@
-class PaulRevereGenerator < Rails::Generator::Base
+require 'rails/generators/active_record/migration'
+
+class PaulRevereGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+  extend ActiveRecord::Generators::Migration
+
   desc "Put the javascript and migration in place"
   source_root File.join(File.dirname(__FILE__), "templates")
 
   def install
-    directory "public/javascripts"
-    file "announcements.js", "public/javascripts/announcements.js"
+    copy_file "announcements.js", "public/javascripts/announcements.js"
     migration_template "migration.rb", "db/migrate/create_announcements.rb"
   end
 end
