e217b855661dd53bb4c04d8748f421c69ceab265
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-07-29 17:45:48 -0400

    Use the .up syntax, instead of migrate(:up) since that's not executing the migrations correctly on rails 3.1.x.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,7 +22,7 @@ end
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 require File.join(PROJECT_ROOT, 'lib', 'generators', 'paul_revere', 'templates', 'migration')
 ActiveRecord::Base.establish_connection(
-  :adapter  => 'sqlite3',
-  :database => ':memory:'
+  :adapter  => "sqlite3",
+  :database => ":memory:"
 )
-CreateAnnouncements.suppress_messages { CreateAnnouncements.migrate(:up) }
+CreateAnnouncements.suppress_messages { CreateAnnouncements.up }
