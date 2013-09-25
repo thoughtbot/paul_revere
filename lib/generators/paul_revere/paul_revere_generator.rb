6cddd44d3badebe666bb78729e7b9cf7d7637c50
Gavin Reynolds <gsreynolds@gmail.com>
2013-09-25 14:44:27 +0100

    Fix generator for ActiveRecord migration in Rails 4. This commit fixes #22.

--- lib/generators/paul_revere/paul_revere_generator.rb
+++ lib/generators/paul_revere/paul_revere_generator.rb
@@ -1,8 +1,7 @@
-require 'rails/generators/active_record/migration'
+require 'rails/generators/active_record'
 
 class PaulRevereGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
-  extend ActiveRecord::Generators::Migration
 
   desc "Put the JavaScript and migration in place"
   source_root File.join(File.dirname(__FILE__), "templates")
@@ -10,6 +9,10 @@ class PaulRevereGenerator < Rails::Generators::Base
   def install
     copy_javascript if needs_js_copied?
     migration_template "migration.rb", "db/migrate/create_announcements.rb"
+  end
+
+  def self.next_migration_number dirname
+      ActiveRecord::Generators::Base.next_migration_number dirname
   end
 
   private
