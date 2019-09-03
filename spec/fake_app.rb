9f2acc5179c96b6e25c31bc8a483530ce9346af8
Matt Jankowski <mjankowski@thoughtbot.com>
2019-09-03 09:51:43 -0400

    Turn on `cache_classes` in fake rails app for specs (avoids rails six deprecation warning)

--- spec/fake_app.rb
+++ spec/fake_app.rb
@@ -7,6 +7,7 @@ module PaulRevere
   class Application < Rails::Application
     config.secret_key_base = "test"
     config.paths["config/database"] = ["spec/support/database.yml"]
+    config.cache_classes = true
     config.eager_load = false
     if config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
       if Rails.gem_version < Gem::Version.new("6.0")
